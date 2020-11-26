4d338ddde12618dea42d207fc5a66b30a0deb252
Manuel <manuel.serrano943@gmail.com>
2020-11-26 17:18:00 +0100

    check winner function and IA movements fixed

--- scripts.js
+++ scripts.js
@@ -3,11 +3,13 @@
 const gameBoardModule = (() => {
 
     const board = [...document.querySelectorAll(".tictactoe-box")];
+    console.log(board[4].textContent);
     const playButton = document.getElementById("play-button");
     const playerName = document.getElementById("player-name");
     const errorInput = document.getElementById("error-input");
+    const winnerDiv = document.getElementById("winner");
     toggleBoard(board, false);
-    
+
     /* Player */
     let player;
     let enemy;
@@ -33,33 +35,94 @@ const gameBoardModule = (() => {
                 boardPlays.push(element);
                 element.innerHTML = "X";
                 printEnemy(board);
+                checkWinner();
             }
         })
     });
 
     function printEnemy(board) {
 
-        for (let index = 0; index < board.length; index++) {
-            if(board[index].textContent != "X" && board[index].textContent != "O") {
-                boardPlays.push(board[index]);
-                board[index].innerHTML = "O";  
+        let movement = Math.floor(Math.random() * (8 - 1) +1);
+        while(board[movement].textContent == "X" || board[movement].textContent == "O") {
+            if(boardPlays.length >= 8) {
                 break;
             }
+            movement = Math.floor(Math.random() * (8 - 1) +1);
+            
         }
+        console.log(movement);
+        console.log(board[movement]);
+        boardPlays.push(board[movement]);
+        board[movement].innerHTML = "O";
     }
 
     function toggleBoard(board, playing) {
-        if(!playing) {
+        if (!playing) {
             board.forEach(element => {
                 element.classList.add("inactive");
             })
         } else {
             board.forEach(element => {
                 element.classList.remove("inactive");
+                element.innerHTML = "";
             })
         }
     }
 
+
+    const horizontal = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+    ];
+
+    const vertical = [
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8]
+    ];
+
+    const tie = [
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    let winCheck = [horizontal, vertical, tie];
+    let win_X = 0;
+    let win_O = 0;
+
+    function checkWinner() {
+
+        winCheck.forEach(cell => {
+            cell.forEach(combo => {
+
+                combo.forEach(secuence => {
+                    if (board[secuence].textContent == "X") {
+                        win_X++;
+                        if (win_X == 3) {
+                            displayWinner(player.getName());
+                        }
+                    } else if (board[secuence].textContent == "O") {
+                        win_O++;
+                        if (win_O == 3) {
+                            displayWinner("IA");
+                        }
+                    }
+                })
+                win_X = 0;
+                win_O = 0;
+            })
+        })
+    }
+
+    function displayWinner(winner) {
+        winnerDiv.innerHTML = winner +" wins";
+        playButton.value = "Play again!";
+        board.forEach(box => {
+            box.classList.add("inactive");
+        })
+        
+    }
 })();
 
 
